03084043b2261c215ab0f21e8bd1ab80252b4510
lizuxian <tanzheng.li@outlook.com>
2024-10-21 23:40:05 +0800

    初始化项目模板

---
+++ src/main/java/com/ling/mianshiya/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.ling.mianshiya.mapper;
+
+import com.ling.mianshiya.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * 
+ * 
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/ling/mianshiya/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.ling.mianshiya.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ling.mianshiya.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
